42ffb8ac813b3eca3b1bd8b0bd09499a7e188e65
Mark Lyck <mark.lyck@gmail.com>
2022-03-12 17:02:11 -0500

    :sparkles: upgrade to react18

--- pages/index/index.page.tsx
+++ pages/index/index.page.tsx
@@ -1,27 +1,20 @@
 import React from 'react'
-import { Counter } from '../../src/components/Counter'
+
+import Hero from './Hero'
+
+const Page = () => (
+  <>
+    <Hero />
+
+    <button
+      bg="blue-400 hover:blue-500 dark:blue-500 dark:hover:blue-600"
+      text="sm white"
+      p="y-2 x-4"
+      border="2 rounded blue-500"
+    >
+      Button
+    </button>
+  </>
+)
 
 export { Page }
-
-function Page() {
-  return (
-    <>
-      <h1>Welcome</h1>
-      This page is:
-      <ul>
-        <li>Rendered to HTML.</li>
-        <li>
-          Interactive. <Counter />
-        </li>
-      </ul>
-      <button
-        bg="blue-400 hover:blue-500 dark:blue-500 dark:hover:blue-600"
-        text="sm white"
-        p="y-2 x-4"
-        border="2 rounded blue-500"
-      >
-        Button
-      </button>
-    </>
-  )
-}
