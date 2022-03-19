e041168d6617eab24f170d568c28632c89aeb86c
Mark Lyck <mark.lyck@gmail.com>
2022-03-19 12:31:13 -0400

    :sparkles: windicss

--- pages/index/index.page.tsx
+++ pages/index/index.page.tsx
@@ -5,15 +5,6 @@ import Hero from './Hero'
 const Page = () => (
   <>
     <Hero />
-
-    <button
-      bg="blue-400 hover:blue-500 dark:blue-500 dark:hover:blue-600"
-      text="sm white"
-      p="y-2 x-4"
-      border="2 rounded blue-500"
-    >
-      Button
-    </button>
   </>
 )
 
