9331871d55cee3961bc98b1f884d5255c9a68bbd
Mark Lyck <mark.lyck@gmail.com>
2022-03-19 22:49:02 -0400

    :lipstick: simplify style

--- pages/index/Hero/index.tsx
+++ pages/index/Hero/index.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <header className="relative">
       <section className="w-full px-6 pb-12 antialiased bg-white">
-        <button bg="blue-400 hover:blue-500 dark:blue-500 dark:hover:blue-600">Button</button>
+        <button bg="blue-400">Button</button>
       </section>
     </header>
   )
